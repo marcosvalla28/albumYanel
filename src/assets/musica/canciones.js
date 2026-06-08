4ecbfa768cb02c2cd765de73c64081b6cb7665b4
marcosvalla28 <markosvalla@hotmail.com>
2026-06-07 22:06:13 -0300

    cargando canciones nuevas

--- src/assets/musica/canciones.js
+++ src/assets/musica/canciones.js
@@ -2,9 +2,13 @@
 import cancion1 from "./cancion1.mp3";
 import cancion2 from "./cancion2.mp3";
 import cancion3 from "./cancion3.mp3";
+import cancion4 from "./cancion4.mp3";
+import cancion5 from "./cancion5.mp3";
 
 export const canciones = [
   { id: 1, titulo: "Te voy a amar", artista: "Axel", src: cancion1 },
   { id: 2, titulo: "Tunel de la vida", artista: "El plan de la Mariposa", src: cancion2 },
   { id: 3, titulo: "Puñaladas", artista: "Lauta", src: cancion3 },
+  { id: 4, titulo: "Estar contigo", artista: "Alex Ubago", src: cancion4 },
+  { id: 5, titulo: "Desde que te tengo", artista: "Carin Leon", src: cancion5 },
 ];
